c5bf36bfcc29de0a963eb6dbd24398bab02476b1
Monakiel <35536198+Monakiel@users.noreply.github.com>
2019-09-17 14:58:02 +0200

    Updates
    Refactoring javascript.js to make it more generic and clearer :)
    
    Added an optional "exit" button (supposedly exiting the console), due to project request.

--- javascript.js
+++ javascript.js
@@ -1,31 +1,40 @@
 // IIFE : Immediately Invoked Function Expression. Evite la pollution de la portée globale. (()=> {JS})()
 (() => {
+
     const data = {
-        twoparts: {
-            start: ['Ne regarde pas', 'Fumer', 'Aimer', 'Manger', 'Respirer'],
-            end: ['le doigt qui pointe la lune.', 'ça tue.', 'à perdre la raison.', 'son derrière.', 'son pet.']
-        },
-        threeparts: {
-            start: ["La bave du crapeau", "Une hirondelle", "Le lièvre", "La douceur du miel", "Seul un idiot", "Qui avale une noix de coco"],
-            middle: ["n'atteint pas", "ne fait pas", "est plus rapide que", "ne console pas", "mesure", "fait confiance à"],
-            end: ["la blanche colombe.", "le printemps.", "la tortue.", "la piqûre de l'abeille", "la profondeur de l'eau avec ses deux pieds.", "son anus."]
-        }
+        start: ["La bave du crapeau", "Une hirondelle", "Le lièvre", "La douceur du miel", "Seul un idiot", "Qui avale une noix de coco"],
+        middle: ["n'atteint pas", "ne fait pas", "est plus rapide que", "ne console pas", "mesure", "fait confiance à"],
+        end: ["la blanche colombe.", "le printemps.", "la tortue.", "la piqûre de l'abeille", "la profondeur de l'eau avec ses deux pieds.", "son anus."]
     }
 
-    const targetDiv = document.getElementById('content')
-    const targetBtn = document.getElementById('generate')
+    const $ = id => document.getElementById(id)
 
-    //Fonction princpale qui va générer les citations aléatoires en prenant des morceaux dans mon objet data.
-    const generateQuote = () => {
-        //val incluent dans la fonction sinon ne change pas les valeurs
-        const valNb = +document.getElementById('nb').value
-        const valType = document.getElementById('type').value
+    const rand = max => Math.floor(Math.random() * max)
+    const start = () => data.start[rand(data.start.length)]
+    const mid = () => data.middle[rand(data.middle.length)]
+    const end = () => data.end[rand(data.end.length)]
+
+    const algos = {
+        twoparts: () => start() + ' et ' + end(),
+
+        threeparts: () => [start(), mid(), end()].join(' '),
+    }
+
+    const targetDiv = $('content')
+    const targetBtn = $('generate')
+
+    // Fonction principale qui va générer les citations aléatoires en prenant des morceaux dans mon objet data.
+    const generateQuotes = () => {
+        // valNb et valType intégrées dans la fonction (et pas en-dehors) sinon ne change pas les valeurs
+        const valNb = +$('nb').value
+        const valType = $('type').value
 
         // Vérification de sécurité pour valType
-        if (!data.hasOwnProperty(valType)) {
-            alert('Valeur incorrecte. Avez-vous modifié la valeur, mister renard ?')
+        if (!algos.hasOwnProperty(valType)) {
+            alert('ERREUR. Avez-vous modifié l\'algo, mister renard ?')
             return
         }
+
         // Idem pour valNb
         if (valNb < 1 || valNb > 5) {
             alert('Valeur incorrecte. Avez-vous modifié la valeur, mister renaux ?')
@@ -33,25 +42,21 @@
         }
 
         const quotes = []
+        const algo = algos[valType]
+
         //génération de 1 à 5 citations, qui seront stockées dans le tableau quotes.
         for (let i = 1; i <= valNb; i++) {
 
-            const quote = []
-            //génération des deux ou trois parties aléatoirement, qui seront stockées dans le tableau quote.
-            Object.keys(data[valType]).forEach(part => {
-                const arrayQuoteParts = data[valType][part]
-                const stringQuotePart = arrayQuoteParts[Math.floor(Math.random() * arrayQuoteParts.length)]
-                quote.push(stringQuotePart)
-            })
+            const quote = algo()
 
             console.log('Citation numéro ' + i + ' : ')
-            console.log(quote.join(' '))
-            quotes.push(quote.join(' '))
+            console.log(quote)
+            quotes.push(quote)
         }
 
         //Affichage des citations stockées dans le tableau quotes. De 1 à 5 en fonction du choix de l'utilisateur.
         targetDiv.innerHTML = quotes.join('<br>')
     }
 
-    targetBtn.addEventListener('click', generateQuote)
+    targetBtn.addEventListener('click', generateQuotes)
 })()
